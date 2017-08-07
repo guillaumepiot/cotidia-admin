2c3fe0ed2bc8365c6698ea176255c641de7e8b5b
Guillaume Piot <guillaume@cotidia.com>
2017-08-07 20:57:05 +0100

    Create generic list view.

--- cotidia/admin/views/generic.py
+++ cotidia/admin/views/generic.py
@@ -5,13 +5,28 @@ from cotidia.admin.utils import StaffPermissionRequiredMixin
 
 class AdminListView(StaffPermissionRequiredMixin, ListView):
     columns = ()
-    template_name = 'admin/generic/list.html'
     paginate_by = 25
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context['column_headers'] = (column[0] for column in self.columns)
-        context['columns'] = (column[1] for column in self.columns)
+        context["column_headers"] = (column[0] for column in self.columns)
+        context["columns"] = (column[1] for column in self.columns)
+
+        context["app_label"] = self.model._meta.app_label
+        context["model_name"] = self.model._meta.model_name
+        context["object_name"] = self.model._meta.object_name
 
         return context
+
+    def get_template_names(self):
+
+        template = "admin/{app}/{model}/list.html".format(
+            app=self.model._meta.app_label,
+            model=self.model._meta.model_name
+        )
+
+        return [
+            template,
+            "admin/generic/page/list.html",
+        ]
