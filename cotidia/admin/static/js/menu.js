1eec8c84e0815d66af2dfbec754b3cbe92def201
Guillaume Piot <guillaume@cotidia.com>
2017-08-22 18:03:46 +0100

    Update style assets. Add menu toggle functionality.

--- cotidia/admin/static/js/menu.js
+++ cotidia/admin/static/js/menu.js
@@ -1,14 +1,29 @@
 (function(){
-  function showMenu(menu) {
+  function showMenu (menu) {
     menu.querySelector('.menu__container').classList.add('menu__container--mobile-active')
   }
 
-  function hideMenu(menu) {
+  function hideMenu (menu) {
     menu.querySelector('.menu__container').classList.remove('menu__container--mobile-active')
   }
 
+  function collapseSidebar () {
+    document.body.classList.add('menu--sidebar-collapse')
+    CookiesHelper.createCookie("cotidiaadminsidebar", 'collapse', 365)
+  }
+
+  function expandSidebar () {
+    document.body.classList.remove('menu--sidebar-collapse')
+    CookiesHelper.eraseCookie("cotidiaadminsidebar")
+  }
+
+  function getSidebarCookieValue () {
+    return document.cookie.replace(/(?:(?:^|.*;\s*)cotidiaadminsidebar\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  }
+
+
   // For each instance of .menu
-  function initMenu() {
+  function initMenu () {
     var menus = document.querySelectorAll('.menu')
     for (i = 0; i < menus.length; i++) {
       var menu = menus[i]
@@ -20,6 +35,13 @@
       menuCloseButton.addEventListener('touchend', hideMenu.bind(null, menu))
       menuCloseButton.addEventListener('click', hideMenu.bind(null, menu))
     }
+    var collapseSidebarBtn = document.querySelector('.menu__item--sidebar-collapse')
+    collapseSidebarBtn.addEventListener('touchend', collapseSidebar)
+    collapseSidebarBtn.addEventListener('click', collapseSidebar)
+
+    var expandSidebarBtn = document.querySelector('.menu__item--sidebar-expand')
+    expandSidebarBtn.addEventListener('touchend', expandSidebar)
+    expandSidebarBtn.addEventListener('click', expandSidebar)
   }
 
   function documentReady () {
@@ -28,7 +50,13 @@
 
   function bootstrap () {
     document.removeEventListener('readystatechange', bootstrap)
+
     initMenu()
+
+    // Automatically add the sidebar collapse class if we are in collapse mode.
+    if (getSidebarCookieValue() == "collapse") {
+      document.body.classList.add('menu--sidebar-collapse')
+    }
   }
 
   // Initialize our menu if the document is ready, otherwise listen to its
