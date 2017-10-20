3a2cbb402a657a0b49182d5c9295fd03cf793b03
Guillaume Piot <guillaume@cotidia.com>
2017-10-20 11:17:38 +0100

    PEP8

--- cotidia/admin/forms.py
+++ cotidia/admin/forms.py
@@ -2,9 +2,10 @@ from django import forms
 
 
 class ActionForm(forms.Form):
-    # actions = forms.ChoiceField(choices=[])
 
     def __init__(self, action_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        print(self.fields)
-        self.fields["actions"] = forms.ChoiceField(choices=action_list, required=False)
+        self.fields["actions"] = forms.ChoiceField(
+            choices=action_list,
+            required=False
+        )
