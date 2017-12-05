bf00634e6c7ede0050a13bcbad7a5bcee4266370
David Clegg <david.clegg@cotidia.com>
2017-12-05 12:44:14 +0000

    Updated tests

--- cotidia/admin/tests/urls.py
+++ cotidia/admin/tests/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls import include, url
 
-from cotidia.admin.tests.views import AdminTestOrderableAPIView, AdminTestSearchDashboardAPIView
+from cotidia.admin.tests.views import (
+        AdminTestOrderableAPIView,
+        AdminTestSearchDashboardAPIView,
+        TestAdminPageView,
+        stub_view
+        )
 
+generic_admin_urls = [
+            url(r'home/$', TestAdminPageView.as_view(), name="test_view"),
+            url(r'stub/(?P<id>[\d]+)$', stub_view, name="genericrecord-list")
+        ]
 
 urlpatterns = [
     url(
@@ -12,5 +21,10 @@ urlpatterns = [
     url(
         r'^api/admin/',
         include("cotidia.admin.urls.api", namespace="generic-api")
+        ),
+    url(r'^test/',
+        include(generic_admin_urls, namespace="generic-admin"),
         )
+
 ]
+
--- cotidia/admin/tests/views.py
+++ cotidia/admin/tests/views.py
@@ -1,4 +1,9 @@
 from cotidia.admin.views.api import AdminOrderableAPIView, AdminSearchDashboardAPIView
+from django.views.generic import View
+from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponse
+from .models import GenericRecord
 
 
 class AdminTestOrderableAPIView(AdminOrderableAPIView):
@@ -7,3 +12,18 @@ class AdminTestOrderableAPIView(AdminOrderableAPIView):
 
 class AdminTestSearchDashboardAPIView(AdminSearchDashboardAPIView):
     pass
+
+
+class TestAdminPageView(View):
+    def get(self, *args, **kwargs):
+        content_type_id = ContentType.objects.get_for_model(GenericRecord).id
+        context = {
+                "app_label": "generic",
+                "model_name": "object",
+                "url_type": "list",
+                "content_type_id": content_type_id,
+                }
+        return render(self.request, "admin/generic/utils/test.html", context)
+
+def stub_view(request, *args, **kwargs):
+    return HttpResponse()
