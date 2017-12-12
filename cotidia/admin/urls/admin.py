0fd07d87b548e55247af911ed1422164f2e78f71
Alex Russell <alex.russell@cotidia.com>
2017-12-12 17:27:34 +0000

    Give generic list URL a name so it can be reversed.

--- cotidia/admin/urls/admin.py
+++ cotidia/admin/urls/admin.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 
-from cotidia.admin.views.generic import AdminOrderableView,  AdminGenericListView
+from cotidia.admin.views.generic import (
+    AdminOrderableView,
+    AdminGenericListView
+)
 
 
 urlpatterns = [
@@ -11,6 +14,7 @@ urlpatterns = [
     ),
     url(
         r'^list/(?P<app_label>[a-zA-Z_]+)/(?P<model>[a-zA-Z_]+)',
-        AdminGenericListView.as_view()
+        AdminGenericListView.as_view(),
+        name='list'
         )
 ]
