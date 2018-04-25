f49d5f5e490213acd5c2b367ee3db4746704c39c
Guillaume Piot <guillaume@cotidia.com>
2018-04-25 11:42:28 +0100

    Add styleguide

---
+++ styleguide/docs/01-index.md
@@ -0,0 +1,5 @@
+---
+title: Cotidia Admin
+---
+
+This is your index page. You can edit its contents at `docs/01-index.hbs`
---
+++ styleguide/fractal.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/*
+* Require the path module
+*/
+const path = require('path');
+
+/*
+ * Require the Fractal module
+ */
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/*
+ * Give your project a title.
+ */
+fractal.set('project.title', 'Cotidia Admin');
+
+/*
+ * Tell Fractal where to look for components.
+ */
+fractal.components.set('path', path.join(__dirname, 'components'));
+fractal.components.set('default.preview', '@preview');
+
+/*
+ * Tell Fractal where to look for documentation pages.
+ */
+fractal.docs.set('path', path.join(__dirname, 'docs'));
+
+/*
+ * Tell the Fractal web preview plugin where to look for static assets.
+ */
+fractal.web.set('static.path', path.join(__dirname, '../cotidia/admin/static/'));
