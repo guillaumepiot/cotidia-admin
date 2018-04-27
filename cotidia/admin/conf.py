dc5e1f1bb98ba2fe2d99b3846542dec82491760b
Guillaume Piot <guillaume@cotidia.com>
2018-04-27 21:52:43 +0100

    Update shortcuts and global search.

--- cotidia/admin/conf.py
+++ cotidia/admin/conf.py
@@ -17,6 +17,7 @@ class AdminConf(AppConf):
 
     # A list of links to go to from the main menu
     SHORTCUTS = []
+    SHORTCUTS_TEXT = "Quick links"
 
     # A list of models and fields to search globally
     GLOBAL_SEARCH = []
--- cotidia/admin/context_processor.py
+++ cotidia/admin/context_processor.py
@@ -8,6 +8,7 @@ def admin_settings(request):
         "SITE_NAME": settings.SITE_NAME,
         "ADMIN_DETAIL_CURRENCY": settings.ADMIN_DETAIL_CURRENCY,
         "ADMIN_SHORTCUTS": settings.ADMIN_SHORTCUTS,
+        "ADMIN_SHORTCUTS_TEXT": settings.ADMIN_SHORTCUTS_TEXT,
         "ADMIN_GLOBAL_SEARCH": settings.ADMIN_GLOBAL_SEARCH,
         "DEBUG": settings.DEBUG
     }
