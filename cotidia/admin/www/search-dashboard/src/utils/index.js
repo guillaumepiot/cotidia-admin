a300df17fd4892313395494c6d4bf274850cefe4
Alex Russell <alex.russell@cotidia.com>
2018-05-23 16:03:59 +0100

    Add debounce and "make promise cancellable" utilities. (We will only use debounce for now.)

--- cotidia/admin/www/search-dashboard/src/utils/index.js
+++ cotidia/admin/www/search-dashboard/src/utils/index.js
@@ -7,3 +7,31 @@ export const messageHandlerFactory = (store) => (event) => {
     store.dispatch(handleSearchDashboardMessage(event.data))
   }
 }
+
+export const makePromiseCancellable = (promise) => {
+  let isCancelled = false
+
+  const newPromise = new Promise(
+    (resolve, reject) => promise.then(
+      (r) => isCancelled ? reject({ isCanceled: true }) : resolve(r)
+    )
+  )
+
+  newPromise.cancel = () => {
+    isCancelled = true
+  }
+
+  return newPromise
+}
+
+export const debounce = (ms, func) => {
+  let timeout
+
+  return (...args) => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+
+    timeout = setTimeout(func, ms, ...args)
+  }
+}
