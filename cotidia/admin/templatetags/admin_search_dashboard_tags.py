aad49cf41d6f3d7e635727be5cde5a99aa7b3a58
David Clegg <david.clegg@cotidia.com>
2018-06-21 13:51:14 +0100

    Adding new dashboard config

--- cotidia/admin/templatetags/admin_search_dashboard_tags.py
+++ cotidia/admin/templatetags/admin_search_dashboard_tags.py
@@ -12,9 +12,9 @@ register = template.Library()
 
 
 @register.inclusion_tag(
-    'admin/partials/search_dashboard_config.py', takes_context=True
+    'admin/partials/search_dashboard_config.html', takes_context=True
 )
-def render_search_dashboard_config2(
+def render_search_dashboard_config(
     context,
     app_label,
     model_name,
@@ -87,66 +87,3 @@ def render_search_dashboard_config2(
         context['default_order_by'] = default_order[0]
 
     return context
-
-@register.simple_tag(takes_context=True)
-def render_search_dashboard_config(
-    context,
-    app_label,
-    model_name,
-    url_type,
-    api_token,
-    default_columns=[],
-    default_filters=[],
-    default_order=[],
-    batch_actions=[],
-    endpoint=None
-):
-    model_class = ContentType.objects.get(
-        app_label=app_label,
-        model=model_name
-    ).model_class()
-    if endpoint is None:
-        endpoint = reverse(
-            'generic-api:object-list',
-            kwargs={
-
-                "app_label": app_label,
-                "model": model_name
-            }
-        )
-
-    model_name = model_class._meta.model_name
-    if app_label and model_name and url_type:
-        # To future maintainers: sorry
-        # This gets the url template by doing a reverse lookup on a url that fits convention
-        # It passes a trash ID and then string replaces it with a template tag for js
-        # TODO find a better way of doing this
-        try:
-            url_name = "{}-admin:{}-{}".format(app_label, model_name, url_type)
-            detail_endpoint = reverse(
-                url_name,
-                kwargs={"pk": 9999})
-            detail_endpoint = detail_endpoint.replace("9999", ":id")
-        except NoReverseMatch:
-            detail_endpoint = None
-    else:
-        detail_endpoint = None
-    if endpoint is None:
-        endpoint = reverse(
-            'generic-api:object-list',
-            kwargs={
-                "app_label": app_label,
-                "model": model_name
-            }
-        )
-
-    return mark_safe(json.dumps(get_model_structure(
-        model_class,
-        endpoint=endpoint,
-        detail_endpoint=detail_endpoint,
-        token=api_token,
-        default_columns=default_columns,
-        default_filters=default_filters,
-        default_order=default_order,
-        batch_actions=batch_actions
-    )))
