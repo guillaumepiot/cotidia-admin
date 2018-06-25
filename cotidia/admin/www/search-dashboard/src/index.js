b30f405080e03d63e1cc43c317e8f09c7886cec6
Alex Russell <alex.russell@cotidia.com>
2018-06-25 10:17:04 +0100

    Remove now-unused import

--- cotidia/admin/www/search-dashboard/src/index.js
+++ cotidia/admin/www/search-dashboard/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
 import { Provider } from 'react-redux'
 import configureStore from './redux/create'
