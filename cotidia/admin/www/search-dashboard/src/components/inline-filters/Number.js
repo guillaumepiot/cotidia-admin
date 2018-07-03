1072e13c3486d6eb656773eb0f27528df2f0723c
Alex Russell <alex.russell@cotidia.com>
2018-07-03 17:19:14 +0100

    Make "inline" number filter handle min/max

--- cotidia/admin/www/search-dashboard/src/components/inline-filters/Number.js
+++ cotidia/admin/www/search-dashboard/src/components/inline-filters/Number.js
@@ -6,26 +6,47 @@ import { TextInput } from '@cotidia/react-ui'
 export default class Number extends Component {
   static propTypes = {
     label: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
     updateValue: PropTypes.func.isRequired,
-    value: PropTypes.string,
+    value: PropTypes.shape({
+      min: PropTypes.string.isRequired,
+      max: PropTypes.string.isRequired,
+    }),
   }
 
-  static defaultProps = {
-    value: '',
-  }
-
-  updateValue = ({ [this.props.name]: value }) => this.props.updateValue(value)
+  updateMin = ({ min }) => this.props.updateValue({ ...this.props.value, min })
+  updateMax = ({ max }) => this.props.updateValue({ ...this.props.value, max })
 
   render () {
+    const { label, value } = this.props
+
     return (
-      <TextInput
-        name={this.props.name}
-        label={this.props.label}
-        type='number'
-        updateValue={this.updateValue}
-        value={this.props.value}
-      />
+      <>
+        <div className='form__group'>
+          <label className='form__label'>{label} is at least</label>
+          <div className='form__control'>
+            <TextInput
+              autoFocus
+              controlOnly
+              name='min'
+              type='number'
+              updateValue={this.updateMin}
+              value={value?.min}
+            />
+          </div>
+        </div>
+        <div className='form__group'>
+          <label className='form__label'>and at most</label>
+          <div className='form__control'>
+            <TextInput
+              controlOnly
+              name='max'
+              type='number'
+              updateValue={this.updateMax}
+              value={value?.max}
+            />
+          </div>
+        </div>
+      </>
     )
   }
 }
