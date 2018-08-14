3443fe5c31cd2dde67748ee441f525ad31f3d65a
Guillaume Piot <guillaume@cotidia.com>
2018-08-14 11:54:12 +0100

    Add generic list view to views import.

--- cotidia/admin/views/__init__.py
+++ cotidia/admin/views/__init__.py
@@ -1,5 +1,6 @@
 from .generic import (
     AdminListView,
+    AdminGenericListView,
     AdminDetailView,
     AdminCreateView,
     AdminUpdateView,
@@ -12,6 +13,7 @@ from .generic import (
 
 __all__ = [
     "AdminListView",
+    "AdminGenericListView",
     "AdminDetailView",
     "AdminCreateView",
     "AdminUpdateView",
