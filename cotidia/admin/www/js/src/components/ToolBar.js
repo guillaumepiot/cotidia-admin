guillaumepiot/cotidia-admin

commit 874fd3368345ffaa3e59bd03d42540637fdb4d80

Add `content-filter__item` container to toolbar filters. Closes #81.

cotidia/admin/www/js/src/components/ToolBar.js
@@ -74,18 +74,20 @@ export default class ToolBar extends Component {
 
     return (
       <>
-        <div className='form__group form__group--boxed'>
-          <TextInput
-            controlOnly
-            label='Search'
-            name='searchTerm'
-            placeholder='Search'
-            prefix={<Icon icon='search' />}
-            type='text'
-            updateValue={this.updateSearchTerm}
-            updateValueOnBlur={false}
-            value={searchTerm}
-          />
+        <div className='content-filter__item'>
+          <div className='form__group form__group--boxed'>
+            <TextInput
+              controlOnly
+              label='Search'
+              name='searchTerm'
+              placeholder='Search'
+              prefix={<Icon icon='search' />}
+              type='text'
+              updateValue={this.updateSearchTerm}
+              updateValueOnBlur={false}
+              value={searchTerm}
+            />
+          </div>
         </div>
 
         {toolbarFilters && toolbarFilters.map((filter) => {
@@ -107,12 +109,14 @@ export default class ToolBar extends Component {
 
           if (Component) {
             return (
-              <Component
-                {...filterProps}
-                key={filterProps.name}
-                updateValue={this.updateFilterValueFactory(filter.name)}
-                value={filters[filter.name]}
-              />
+              <div className='content-filter__item'>
+                <Component
+                  {...filterProps}
+                  key={filterProps.name}
+                  updateValue={this.updateFilterValueFactory(filter.name)}
+                  value={filters[filter.name]}
+                />
+              </div>
             )
           }
         })}
@@ -182,7 +186,7 @@ export default class ToolBar extends Component {
 
     return (
       <div className='content__toolbar'>
-        <div className='content__filter'>
+        <div className='content__filter content-filter'>
           {anyResultsSelected ? this.renderBatchActionToolbar() : this.renderSearchToolbar()}
         </div>
 

commit 81bf4ec372bc4ea87f6c2dd2b9a629bb665d3551

Fix issue introduced into toolbar in 874fd3368345ffaa3e59bd03d42540637fdb4d80.

cotidia/admin/www/js/src/components/ToolBar.js
@@ -113,10 +113,9 @@ export default class ToolBar extends Component {
 
           if (Component) {
             return (
-              <div className='content-filter__item'>
+              <div className='content-filter__item' key={filterProps.name}>
                 <Component
                   {...filterProps}
-                  key={filterProps.name}
                   updateValue={this.updateFilterValueFactory(filter.name)}
                   value={filters[filter.name]}
                 />