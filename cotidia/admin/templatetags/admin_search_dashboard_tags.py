9780eeb96447952a7bf2bca36b17c384196960c9
Guillaume Piot <guillaume@cotidia.com>
2018-11-27 14:28:50 +0000

    Blackify

--- cotidia/admin/templatetags/admin_search_dashboard_tags.py
+++ cotidia/admin/templatetags/admin_search_dashboard_tags.py
@@ -10,9 +10,7 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 
-@register.inclusion_tag(
-    'admin/partials/dynamic_list_config.html', takes_context=False
-)
+@register.inclusion_tag("admin/partials/dynamic_list_config.html", takes_context=False)
 def render_search_dashboard_config(
     app_label,
     model_name,
@@ -24,16 +22,10 @@ def render_search_dashboard_config(
     default_filters=None,
     batch_actions=None,
 ):
-    context = {
-        'app_label': app_label,
-        'model_name': model_name,
-    }
+    context = {"app_label": app_label, "model_name": model_name}
 
     # Get model class and its corresponding serializer class.
-    model = ContentType.objects.get(
-        app_label=app_label,
-        model=model_name
-    ).model_class()
+    model = ContentType.objects.get(app_label=app_label, model=model_name).model_class()
 
     if not serializer:
         # Double call here because `serializer()` gets the serializer class,
@@ -42,7 +34,7 @@ def render_search_dashboard_config(
 
         # TODO: Refactor this so that the method is `get_serializer_class` or
         #       something like that.
-        serializer = model.SearchProvider.serializer()()
+        serializer = model.SearchProvider.dynamic_list_serializer()()
 
     # Calculate API endpoint
     if not endpoint:
@@ -54,7 +46,7 @@ def render_search_dashboard_config(
 
     # Calculate default_oder_by
     if not default_order_by:
-        default_order_by = serializer.get_option('default_order_by')
+        default_order_by = serializer.get_option("default_order_by")
 
     field_representation = serializer.get_field_representation()
     column_representation = {}
@@ -68,63 +60,61 @@ def render_search_dashboard_config(
             filter_representation[name]["label"] = field["label"]
             if field.get("configuration"):
                 filter_representation[name]["configuration"] = field["configuration"]
-            query_param = (filter_representation.get("queryParameter") or name)
+            query_param = filter_representation.get("queryParameter") or name
             filter_representation[name]["queryParameter"] = query_param
 
-    extra_filters = serializer.get_option('extra_filters')
+    extra_filters = serializer.get_option("extra_filters")
     if extra_filters:
-        for name, field in serializer.get_option('extra_filters').items():
+        for name, field in serializer.get_option("extra_filters").items():
             filter_representation[name] = field
 
-    context['verbose_name'] = model._meta.verbose_name
-    context['verbose_name_plural'] = model._meta.verbose_name_plural
+    context["verbose_name"] = model._meta.verbose_name
+    context["verbose_name_plural"] = model._meta.verbose_name_plural
 
-    context['columns'] = serializer.get_columns()
-    context['default_columns'] = default_columns
-    context['default_order_by'] = default_order_by
-    context['default_filters'] = default_filters
+    context["columns"] = serializer.get_columns()
+    context["default_columns"] = default_columns
+    context["default_order_by"] = default_order_by
+    context["default_filters"] = default_filters
 
-    context['endpoint'] = endpoint
-    context['auth_token'] = auth_token
-    context['title'] = serializer.get_option('title', default=model._meta.verbose_name_plural.title())
-    context['field_representation'] = column_representation
-    context['filters'] = filter_representation
+    context["endpoint"] = endpoint
+    context["auth_token"] = auth_token
+    context["title"] = serializer.get_option(
+        "title", default=model._meta.verbose_name_plural.title()
+    )
+    context["field_representation"] = column_representation
+    context["filters"] = filter_representation
 
     # Get some config values
-    context['primary_color'] = serializer.get_option('primary_color')
-    context['date_format'] = serializer.get_option('date_format')
-    context['datetime_format'] = serializer.get_option('datetime_format')
-    context['week_day_start'] = serializer.get_option('week_day_start')
-    context['table_striped'] = serializer.get_option('table_striped')
-    context['columns_configurable'] = serializer.get_option(
-        'columns_configurable'
-    )
-    context['search_visible'] = True
+    context["primary_color"] = serializer.get_option("primary_color")
+    context["date_format"] = serializer.get_option("date_format")
+    context["datetime_format"] = serializer.get_option("datetime_format")
+    context["week_day_start"] = serializer.get_option("week_day_start")
+    context["table_striped"] = serializer.get_option("table_striped")
+    context["columns_configurable"] = serializer.get_option("columns_configurable")
+    context["search_visible"] = True
 
     # Stuff passed straight from the serializer.
-    context['list_handling'] = serializer.get_option('list_handling')
-    context['extra_filters'] = serializer.get_option('extra_filters')
-    context['toolbar_filters'] = serializer.get_option('toolbar_filters')
-    context['sidebar_filters'] = serializer.get_option('sidebar_filters')
-    context['global_actions'] = serializer.get_option('global_actions')
-    context['categorise_by'] = serializer.get_option('categorise_by')
-    context['list_fields'] = serializer.get_option('list_fields')
-    context['sidebar_starts_shown'] = serializer.get_option('sidebar_starts_shown')
-    context['ignore_stored_config'] = serializer.get_option('ignore_stored_config')
-    context['detail_url_field'] = serializer.get_detail_url_field()
+    context["list_handling"] = serializer.get_option("list_handling")
+    context["extra_filters"] = serializer.get_option("extra_filters")
+    context["toolbar_filters"] = serializer.get_option("toolbar_filters")
+    context["sidebar_filters"] = serializer.get_option("sidebar_filters")
+    context["global_actions"] = serializer.get_option("global_actions")
+    context["categorise_by"] = serializer.get_option("categorise_by")
+    context["list_fields"] = serializer.get_option("list_fields")
+    context["sidebar_starts_shown"] = serializer.get_option("sidebar_starts_shown")
+    context["ignore_stored_config"] = serializer.get_option("ignore_stored_config")
+    context["detail_url_field"] = serializer.get_detail_url_field()
 
     # Batch actions can be overridden by the caller, so allow for that.
     if batch_actions:
-        context['batch_actions'] = batch_actions
+        context["batch_actions"] = batch_actions
     else:
-        context['batch_actions'] = serializer.get_option('batch_actions')
+        context["batch_actions"] = serializer.get_option("batch_actions")
 
     return context
 
 
-@register.inclusion_tag(
-    'admin/partials/dynamic_list_config.html', takes_context=False
-)
+@register.inclusion_tag("admin/partials/dynamic_list_config.html", takes_context=False)
 def render_dynamic_list_config(
     app_label,
     model_name,
@@ -136,16 +126,10 @@ def render_dynamic_list_config(
     default_filters=None,
     batch_actions=None,
 ):
-    context = {
-        'app_label': app_label,
-        'model_name': model_name,
-    }
+    context = {"app_label": app_label, "model_name": model_name}
 
     # Get model class and its corresponding serializer class.
-    model = ContentType.objects.get(
-        app_label=app_label,
-        model=model_name
-    ).model_class()
+    model = ContentType.objects.get(app_label=app_label, model=model_name).model_class()
 
     if not serializer:
         # Double call here because `serializer()` gets the serializer class,
@@ -154,7 +138,7 @@ def render_dynamic_list_config(
 
         # TODO: Refactor this so that the method is `get_serializer_class` or
         #       something like that.
-        serializer = model.SearchProvider.serializer()()
+        serializer = model.SearchProvider.dynamic_list_serializer()()
 
     # Calculate API endpoint
     if not endpoint:
@@ -166,60 +150,64 @@ def render_dynamic_list_config(
 
     # Calculate default_oder_by
     if not default_order_by:
-        default_order_by = serializer.get_option('default_order_by')
+        default_order_by = serializer.get_option("default_order_by")
 
-    context['verbose_name'] = model._meta.verbose_name
-    context['verbose_name_plural'] = model._meta.verbose_name_plural
+    context["verbose_name"] = model._meta.verbose_name
+    context["verbose_name_plural"] = model._meta.verbose_name_plural
 
-    context['columns'] = serializer.get_columns()
-    context['filters'] = serializer.get_filter_representation()
-    context['default_columns'] = default_columns
-    context['default_order_by'] = default_order_by
-    context['default_filters'] = default_filters
+    context["columns"] = serializer.get_columns()
+    context["filters"] = serializer.get_filter_representation()
+    context["default_columns"] = default_columns
+    context["default_order_by"] = default_order_by
+    context["default_filters"] = default_filters
 
-    context['endpoint'] = endpoint
-    context['auth_token'] = auth_token
-    context['title'] = serializer.get_option('title', default=model._meta.verbose_name_plural.title())
-    context['field_representation'] = serializer.get_field_representation()
+    context["endpoint"] = endpoint
+    context["auth_token"] = auth_token
+    context["title"] = serializer.get_option(
+        "title", default=model._meta.verbose_name_plural.title()
+    )
+    context["field_representation"] = serializer.get_field_representation()
 
     # Get some config values
-    context['primary_color'] = serializer.get_option('primary_color')
-    context['date_format'] = serializer.get_option('date_format')
-    context['datetime_format'] = serializer.get_option('datetime_format')
-    context['week_day_start'] = serializer.get_option('week_day_start')
-    context['table_striped'] = serializer.get_option('table_striped')
-    context['columns_configurable'] = serializer.get_option(
-        'columns_configurable'
+    context["primary_color"] = serializer.get_option("primary_color")
+    context["date_format"] = serializer.get_option("date_format")
+    context["datetime_format"] = serializer.get_option("datetime_format")
+    context["week_day_start"] = serializer.get_option("week_day_start")
+    context["table_striped"] = serializer.get_option("table_striped")
+    context["columns_configurable"] = serializer.get_option("columns_configurable")
+    context["search_visible"] = serializer.get_option("search_visible", True)
+    context["filter_tag_bar_visible"] = serializer.get_option(
+        "filter_tag_bar_visible", False
     )
-    context['search_visible'] = serializer.get_option('search_visible', True)
-    context['filter_tag_bar_visible'] = serializer.get_option('filter_tag_bar_visible', False)
 
     # Stuff passed straight from the serializer.
-    context['list_handling'] = serializer.get_option('list_handling')
-    context['extra_filters'] = serializer.get_option('extra_filters')
-    context['toolbar_filters'] = serializer.get_option('toolbar_filters')
-    context['sidebar_filters'] = serializer.get_option('sidebar_filters')
-    context['global_actions'] = serializer.get_option('global_actions')
-    context['categorise_by'] = serializer.get_option('categorise_by')
-    context['list_fields'] = serializer.get_option('list_fields')
-    context['sidebar_starts_shown'] = serializer.get_option('sidebar_starts_shown')
-    context['ignore_stored_config'] = serializer.get_option('ignore_stored_config')
-    context['filter_suggest_configuration'] = serializer.get_option('filter_suggest_configuration')
-    context['allowed_results_modes'] = serializer.get_option('allowed_results_modes')
-    context['default_results_mode'] = serializer.get_option('default_results_mode')
-    context['map_configuration'] = serializer.get_option('map_configuration')
-    context['default_per_page'] = serializer.get_option('default_per_page')
-    context['detail_url_field'] = serializer.get_detail_url_field()
+    context["list_handling"] = serializer.get_option("list_handling")
+    context["extra_filters"] = serializer.get_option("extra_filters")
+    context["toolbar_filters"] = serializer.get_option("toolbar_filters")
+    context["sidebar_filters"] = serializer.get_option("sidebar_filters")
+    context["global_actions"] = serializer.get_option("global_actions")
+    context["categorise_by"] = serializer.get_option("categorise_by")
+    context["list_fields"] = serializer.get_option("list_fields")
+    context["sidebar_starts_shown"] = serializer.get_option("sidebar_starts_shown")
+    context["ignore_stored_config"] = serializer.get_option("ignore_stored_config")
+    context["filter_suggest_configuration"] = serializer.get_option(
+        "filter_suggest_configuration"
+    )
+    context["allowed_results_modes"] = serializer.get_option("allowed_results_modes")
+    context["default_results_mode"] = serializer.get_option("default_results_mode")
+    context["map_configuration"] = serializer.get_option("map_configuration")
+    context["default_per_page"] = serializer.get_option("default_per_page")
+    context["detail_url_field"] = serializer.get_detail_url_field()
 
     # Batch actions can be overridden by the caller, so allow for that.
     if batch_actions:
-        context['batch_actions'] = batch_actions
+        context["batch_actions"] = batch_actions
     else:
-        context['batch_actions'] = serializer.get_option('batch_actions')
+        context["batch_actions"] = serializer.get_option("batch_actions")
 
     return context
 
 
-@register.filter(name='json')
+@register.filter(name="json")
 def json_dumps(data):
     return json.dumps(data, cls=DjangoJSONEncoder)
--- cotidia/admin/urls/admin.py
+++ cotidia/admin/urls/admin.py
@@ -2,43 +2,31 @@ from django.urls import path
 
 from cotidia.admin.views.generic import (
     AdminOrderableView,
-    AdminGenericListView,
     AdminGenericSearchView,
-    AdminGenericExportView
+    AdminGenericExportView,
+    DynamicListView,
 )
 
 app_name = "cotidia.admin"
 
 urlpatterns = [
     path(
-        'order/<int:content_type_id>/<int:object_id>',
+        "order/<int:content_type_id>/<int:object_id>",
         AdminOrderableView.as_view(),
-        name='order'
+        name="order",
     ),
+    path("list/<str:app_label>/<str:model>", DynamicListView.as_view(), name="list"),
+    path("search", AdminGenericSearchView.as_view(), name="search"),
     path(
-        'list/<str:app_label>/<str:model>',
-        AdminGenericListView.as_view(),
-        name='list'
-    ),
-    path(
-        'search',
-        AdminGenericSearchView.as_view(),
-        name='search'
-    ),
-    path(
-        'export/<str:app_label>/<str:model>/csv',
+        "export/<str:app_label>/<str:model>/csv",
         AdminGenericExportView.as_view(),
-        {
-            'format': 'csv',
-        },
-        name='export-csv'
+        {"format": "csv"},
+        name="export-csv",
     ),
     path(
-        'export/<str:app_label>/<str:model>/pdf',
+        "export/<str:app_label>/<str:model>/pdf",
         AdminGenericExportView.as_view(),
-        {
-            'format': 'pdf',
-        },
-        name='export-pdf'
+        {"format": "pdf"},
+        name="export-pdf",
     ),
 ]
